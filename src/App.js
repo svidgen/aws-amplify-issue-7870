672c4b46c44a45878689894ed63d3cea383c956b
Jon Wire <wirej@amazon.com>
2021-03-16 14:47:04 -0500

    working sample of css image from s3 using CORS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import Amplify, { Storage } from "aws-amplify";
+import { withAuthenticator } from "aws-amplify-react";
+import awsConfig from "./aws-exports";
+import logo from "./logo.svg";
+import "./App.css";
+
+Amplify.configure(awsConfig);
+
+const imageName = "myImage";
 
 function App() {
+  const [status, setStatus] = useState('Waiting for upload.');
+  const [myImage, setMyImage] = useState(undefined);
+
+  useEffect(() => {
+    if (myImage === undefined) {
+      Storage.get(imageName, { level: "protected" })
+        .then(setMyImage)
+        .catch(err => console.error('could not generate image URL', err))
+      ;
+    } else {
+      console.log("image loaded", myImage);
+    }
+  }, [status]);
+
+  async function onImageChange(e) {
+    const file = e.target.files[0];
+    try {
+      setStatus("Uploading ...");
+      await Storage.put(imageName, file, { level: "protected" });
+      setStatus("Upload complete.");
+    } catch (err) {
+      setStatus("Ruhoh. That didn't work! Try again.");
+      console.log(err);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>{status}</div>
+      <div>
+        Protected upload: <input type='file' onChange={onImageChange} />
+      </div>
+      <img src={myImage} />
+      <div style={{
+        width: "200px",
+        height: "200px",
+        WebkitMaskImage: "url('" + myImage + "')"
+      }}></div>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
